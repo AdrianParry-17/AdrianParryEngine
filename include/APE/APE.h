5b0a2f6d35d47cf445b56f94eeed5dd24c7e5014
AdrianParry-17 <minhhung01006@gmail.com>
2025-07-03 20:40:07 +0700

    Create APE.h

---
+++ include/APE/APE.h
@@ -0,0 +1,14 @@
+#ifndef __APE_H__
+#define __APE_H__
+
+namespace APE {
+  /// @brief Initialize the APE, must call before using the APE. If this fail,
+  /// you shouldn't continue using the APE.
+  /// @return true on success, false on failure.
+  bool Initialize();
+
+  /// @brief Deinitialize the APE and release the resources.
+  void Deinitialize();
+}
+
+#endif // __APE_H__
