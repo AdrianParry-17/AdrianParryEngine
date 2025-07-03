7c87dc216b05a5333b1a6f1b921df471cc889134
AdrianParry-17 <minhhung01006@gmail.com>
2025-07-03 20:42:50 +0700

    Create APE.cpp

---
+++ src/APE.cpp
@@ -0,0 +1,15 @@
+#include "APE/APE.h"
+
+#include <SDL2/SDL.h>
+
+bool APE::Initialize() {
+  // Initialize SDL2
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    return false;
+
+  return true;
+}
+
+void APE::Deinitialize() {
+  SDL_Quit();
+}
